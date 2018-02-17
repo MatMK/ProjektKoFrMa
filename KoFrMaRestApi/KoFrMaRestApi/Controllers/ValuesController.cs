6a6a6caf86bd4cfcc5a34c928ab4783095c7d726
Petr <pema65@seznam.cz>
2018-02-17 15:38:30 +0100

    Values controller

--- KoFrMaRestApi/KoFrMaRestApi/Controllers/ValuesController.cs
+++ KoFrMaRestApi/KoFrMaRestApi/Controllers/ValuesController.cs
@@ -12,9 +12,34 @@ namespace KoFrMaRestApi.Controllers
     {
         //https://www.youtube.com/watch?v=TcovfE8IsHs
 
+
+        // Get api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+         // Get api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // Delete api/values/5
         public void Delete(int id)
         {
 
         }
+
+        // Post api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // Put api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+     
     }
 }
