2964d16688e43c91d6de6c81800bb0e9c1539bac
MatějFrnka <frnkamatej@sssvt.cz>
2018-02-18 10:55:32 +0100

    Základy DaemonController

--- KoFrMaRestApi/KoFrMaRestApi/Controllers/DaemonController.cs
+++ KoFrMaRestApi/KoFrMaRestApi/Controllers/DaemonController.cs
@@ -7,12 +7,23 @@ using System.Net.Http;
 using System.Web.Http;
 using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
-
+using KoFrMaRestApi.Models;
 
 namespace KoFrMaRestApi.Controllers
 {
     public class DaemonController : ApiController
     {
+        [Route("api/Daemon/Register/{daemon}")]
+        [HttpPost]
+        public void DaemonRegister(DaemonInfo daemon)
+        {
 
+        }
+        [Route("api/Daemon/GetInstructions/{DaemonId:int}")]
+        [HttpGet]
+        public Instructions GetInstructions(int DaemonId)
+        {
+            throw new Exception();
+        }
     }
 }
---
+++ KoFrMaRestApi/KoFrMaRestApi/Models/Instructions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KoFrMaRestApi.Models
+{
+    /// <summary>
+    /// Obsahuje instrukce pro Daemon
+    /// </summary>
+    public class Instructions
+    {
+
+    }
+}
