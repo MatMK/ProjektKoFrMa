8657b1e775faf43e30a73d3ecb15084990b27577
MatějFrnka <frnkamatej@sssvt.cz>
2018-02-19 18:52:54 +0100

    připojení daemonu k serveru

---
+++ kofrmadaemon/kofrmadaemon/connectiontoserver/connection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+
+
+namespace KoFrMaDaemon.ConnectionToServer
+{
+    public class Connection
+    {
+        public void PostRequest()
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:50576/api/Daemon/GetInstructions");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = @"{
+	Version: 1,
+	Os: 1,
+	PC_Unique: '2'
+}";
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string reslut;
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                reslut = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
