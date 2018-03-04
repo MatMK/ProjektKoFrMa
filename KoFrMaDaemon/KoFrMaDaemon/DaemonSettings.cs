9e7cfe727e6eb7ffc4fd286507cbcee12c8de4b5
Matěj Kocourek <matej@kocourek.cz>
2018-03-04 22:15:55 +0100

    Vytvořen offline konfigurátor daemona

---
+++ KoFrMaDaemon/KoFrMaDaemon/DaemonSettings.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KoFrMaDaemon
+{
+    public class DaemonSettings
+    {
+        public string ServerIP;
+        public Int64 Password;
+        public string LocalLogPath;
+
+        private StreamReader r;
+        private StreamWriter w;
+
+        public DaemonSettings()
+        {
+            try
+            {
+                if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\KoFrMa\config.ini"))
+                {
+                    r = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\KoFrMa\config.ini");
+                    while (!r.EndOfStream)
+                    {
+                        string tmpRow = r.ReadLine();
+                       if (tmpRow.StartsWith("ServerIP="))
+                       {
+                           this.ServerIP = tmpRow.Substring(9);
+                       }
+                       else if (tmpRow.StartsWith("Password="))
+                       {
+                           this.Password = Convert.ToInt64(tmpRow.Substring(9));
+                       }
+                       else if (tmpRow.StartsWith("LocalLogPath="))
+                       {
+                           this.LocalLogPath = tmpRow.Substring(13);
+                       }
+                }
+                r.Close();
+                r.Dispose();
+                }
+                
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ KoFrMaLocalDaemonConfig/KoFrMaLocalDaemonConfig/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace KoFrMaLocalDaemonConfig
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadIniFile();
+            CheckIfChecked();
+
+        }
+
+        private void checkBox_showPath_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckIfChecked();
+        }
+        private void CheckIfChecked()
+        {
+            if (checkBox_showPath.Checked)
+            {
+                this.textBox_LogPath.Visible = true;
+            }
+            else
+            {
+                this.textBox_LogPath.Visible = false;
+            }
+        }
+        private void LoadIniFile()
+        {
+            try
+            {
+                StreamReader r = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\KoFrMa\config.ini");
+                while (!r.EndOfStream)
+                {
+                    string tmpRow = r.ReadLine();
+                    if (tmpRow.StartsWith("ServerIP="))
+                    {
+                        this.textBox_ServerIP.Text = tmpRow.Substring(9);
+                    }
+                    else if (tmpRow.StartsWith("Password="))
+                    {
+                        this.textBox_Password.Text = tmpRow.Substring(9);
+                    }
+                    else if (tmpRow.StartsWith("LocalLogPath="))
+                    {
+                        if (tmpRow.Substring(13) != "")
+                        {
+                            this.checkBox_showPath.Checked = true;
+                            this.textBox_LogPath.Text = tmpRow.Substring(13);
+                        }
+                        else
+                        {
+                            this.checkBox_showPath.Checked = false;
+                        }
+
+                    }
+                }
+                r.Close();
+                r.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            this.LoadIniFile();
+        }
+
+        private void button_apply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\KoFrMa\config.ini"))
+                {
+                    Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\KoFrMa\");
+                    File.Create(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\KoFrMa\config.ini");
+                }
+                StreamWriter w = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\KoFrMa\config.ini");
+                w.WriteLine("ServerIP=" + this.textBox_ServerIP.Text);
+                w.WriteLine("Password=" + this.textBox_Password.Text);
+                w.WriteLine("LocalLogPath=" + this.textBox_LogPath.Text);
+                w.Close();
+                w.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
