79b6b8076a58f42528e13996d5b8c0ef301fc02d
Petr <pema65@seznam.cz>
2018-03-06 19:13:18 +0100

    changes

--- KoFrMaRestApi/KoFrMaRestApi/EmailSender/EmailSender.cs
+++ KoFrMaRestApi/KoFrMaRestApi/EmailSender/EmailSender.cs
@@ -17,44 +17,6 @@ namespace KoFrMaRestApi.EmailSender
         string Ssubject = "Test";
         string Sbody = "Testing email";
         string SemailTo = "machpetr@sssvt.cz";
-        //string smtpAddress = "smtp.gmail.com";
-        //int portNumber = 587;
-        //bool enableSSL = true;
-        //string emailFrom = "companyemail";
-        //string password = "password";
-        //string emailTo = "Your email";
-        //string subject = "Hello!";
-        //string body = "Hello, Mr.";
-        //MailMessage mail = new MailMessage();
-        //mail.From = new MailAddress(emailFrom);
-        //mail.To.Add(emailTo);
-        //mail.Subject = subject;
-        //mail.Body = body;
-        //mail.IsBodyHtml = true;
-        //using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
-        //{
-        //smtp.Credentials = new NetworkCredential(emailFrom, password);
-        //smtp.EnableSsl = enableSSL;
-        //smtp.Send(mail);
-        //}
-
-        //MailMessage mm = new MailMessage(txtEmail.Text, txtTo.Text);
-        //mm.Subject = txtSubject.Text;
-        //mm.Body = txtBody.Text;
-        //if (fuAttachment.HasFile)//file upload select or not
-        //{
-        //string FileName = Path.GetFileName(fuAttachment.PostedFile.FileName);
-        //mm.Attachments.Add(new Attachment(fuAttachment.PostedFile.InputStream, FileName));
-        //}
-        //mm.IsBodyHtml = false;
-        //SmtpClient smtp = new SmtpClient();
-        //smtp.Host = "smtp.gmail.com";
-        //smtp.EnableSsl = true;
-        //NetworkCredential NetworkCred = new NetworkCredential(txtEmail.Text, txtPassword.Text);
-        //smtp.UseDefaultCredentials = true;
-        //smtp.Credentials = NetworkCred;
-        //smtp.Port = 587;
-        //smtp.Send(mm);
 
         public void SendEmail()
         {
