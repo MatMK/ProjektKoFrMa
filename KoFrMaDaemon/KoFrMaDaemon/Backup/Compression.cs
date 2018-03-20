c1c59cf40fcfed630bf6fb877778528a2259b331
Petr <pema65@seznam.cz>
2018-03-20 01:46:52 +0100

    Few Backup+Compression changes

--- KoFrMaDaemon/KoFrMaDaemon/Backup/Compression.cs
+++ KoFrMaDaemon/KoFrMaDaemon/Backup/Compression.cs
@@ -11,32 +11,22 @@ namespace KoFrMaDaemon.Backup
 {
     public class Compression
     {
-        public void CompressToZip(string source, string destination, byte? compressionLevel)
-        {
-            ServiceKoFrMa.debugLog.WriteToLog("Compressing now...",6);
-            if (!(compressionLevel==null))
-            {
-                ZipFile.CreateFromDirectory(source, destination, (CompressionLevel)compressionLevel, false);
-            }
-            else
-            {
-                ServiceKoFrMa.debugLog.WriteToLog("Compression level is not set! Cannot continue!", 2);
-            }
-            
-        }
-
-        public void CompressTo7z(string PathTo7zFolder, string source, string destination, byte? compressionLevel)
+        public void CompressToZip(string source, string destination, byte compressionLevel)
         {
             ServiceKoFrMa.debugLog.WriteToLog("Compressing now...", 6);
-            if (PathTo7zFolder == null || PathTo7zFolder =="")
+            ZipFile.CreateFromDirectory(source, destination, (CompressionLevel)compressionLevel, false);
+        }
+
+        public void CompressTo7z(string PathTo7zFolder, string source, string destination, byte compressionLevel)
+        {
+            ServiceKoFrMa.debugLog.WriteToLog("Compressing now...", 6);
+            if (PathTo7zFolder == null || PathTo7zFolder == "")
             {
                 ServiceKoFrMa.debugLog.WriteToLog("7-zip is not installed or the path to it is not entered in the .ini file. Cannot compress.", 2);
             }
             else
             {
-                if (!(compressionLevel == null))
-                {
-                    ServiceKoFrMa.debugLog.WriteToLog("Running 7-zip as follows: "+ Path.Combine(PathTo7zFolder, "7z.exe") + " a -t7z " + destination + ' ' + source + "* -mmt -mx" + compressionLevel, 8);
+                    ServiceKoFrMa.debugLog.WriteToLog("Running 7-zip as follows: " + Path.Combine(PathTo7zFolder, "7z.exe") + " a -t7z " + destination + ' ' + source + "* -mmt -mx" + compressionLevel, 8);
                     Process p = new Process();
                     p.StartInfo.FileName = Path.Combine(PathTo7zFolder, "7z.exe");
                     p.StartInfo.Arguments = "a -t7z " + destination + ' ' + source + "* -mmt -mx" + compressionLevel;
@@ -44,16 +34,11 @@ namespace KoFrMaDaemon.Backup
                     p.Start();
                     p.PriorityClass = ProcessPriorityClass.BelowNormal;
                     p.WaitForExit();
-                }
-                else
-                {
-                    ServiceKoFrMa.debugLog.WriteToLog("Compression level is not set! Cannot continue!", 2);
-                }
 
             }
         }
 
-        public void CompressToRar(string PathToRarFolder, string source, string destination, byte? compressionLevel)
+        public void CompressToRar(string PathToRarFolder, string source, string destination, byte compressionLevel)
         {
             ServiceKoFrMa.debugLog.WriteToLog("Compressing now...", 6);
             if (PathToRarFolder == null || PathToRarFolder == "")
@@ -62,21 +47,15 @@ namespace KoFrMaDaemon.Backup
             }
             else
             {
-                if (!(compressionLevel == null))
-                {
-                    ServiceKoFrMa.debugLog.WriteToLog("Running WinRar as follows: " + Path.Combine(PathToRarFolder, "Rar.exe") + " a -r -s -ma5 -m"+compressionLevel + ' ' + destination + ' ' + source, 8);
-                    Process p = new Process();
-                    p.StartInfo.FileName = Path.Combine(PathToRarFolder, "Rar.exe");
-                    p.StartInfo.Arguments = "a -r -s -ma5 -ep1 -m" + compressionLevel + ' ' + destination + ' ' + source;
-                    p.StartInfo.CreateNoWindow = true;
-                    p.Start();
-                    p.PriorityClass = ProcessPriorityClass.BelowNormal;
-                    p.WaitForExit();
-                }
-                else
-                {
-                    ServiceKoFrMa.debugLog.WriteToLog("Compression level is not set! Cannot continue!", 2);
-                }
+
+                ServiceKoFrMa.debugLog.WriteToLog("Running WinRar as follows: " + Path.Combine(PathToRarFolder, "Rar.exe") + " a -r -s -ma5 -m" + compressionLevel + ' ' + destination + ' ' + source, 8);
+                Process p = new Process();
+                p.StartInfo.FileName = Path.Combine(PathToRarFolder, "Rar.exe");
+                p.StartInfo.Arguments = "a -r -s -ma5 -ep1 -m" + compressionLevel + ' ' + destination + ' ' + source;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.PriorityClass = ProcessPriorityClass.BelowNormal;
+                p.WaitForExit();
 
             }
         }
