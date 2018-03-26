536fc8e29d60e32ed6fbdcae4f2c9eeb843cc8ae
Matěj Kocourek <matej@kocourek.cz>
2018-03-26 22:01:29 +0200

    connectioooooon

--- kofrmadaemon/kofrmadaemon/connectiontoserver/connection.cs
+++ kofrmadaemon/kofrmadaemon/connectiontoserver/connection.cs
@@ -13,12 +13,12 @@ namespace KoFrMaDaemon.ConnectionToServer
 {
     public class Connection
     {
-        public List<Task> PostRequest(int[] TasksId, List<int> JournalNotNeeded)
+        public List<Task> PostRequest(List<TaskVersion> currentTasks, List<int> journalNotNeeded,List<TaskComplete> completedTasks)
         {
             ServiceKoFrMa.debugLog.WriteToLog("Creating request to server...", 7);
-            Request request = new Request() {IdTasks = TasksId, BackupJournalNotNeeded = JournalNotNeeded};
+            Request request = new Request() {TasksVersions=currentTasks,BackupJournalNotNeeded = journalNotNeeded,CompletedTasks = completedTasks};
             var httpWebRequest = (HttpWebRequest)WebRequest.Create(ConnectionInfo.ServerURL + @"/api/Daemon/GetInstructions");
-            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.ContentType = "application/base64";
             httpWebRequest.Method = "POST";
 
             ///Timeout aby se aplikace nesekla když se nepřipojí, potom předělat na nastevní hodnoty ze serveru
@@ -28,8 +28,10 @@ namespace KoFrMaDaemon.ConnectionToServer
             ServiceKoFrMa.debugLog.WriteToLog("Trying to send request to server at address " + ConnectionInfo.ServerURL + @"/api/Daemon/GetInstructions", 5);
             using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
+                
                 string json = JsonConvert.SerializeObject(request);
-                streamWriter.Write(json);
+                string jsonBase64 = this.EncodeBase64(System.Text.Encoding.UTF8, json);
+                streamWriter.Write(jsonBase64);
                 streamWriter.Flush();
                 streamWriter.Close();
             }
@@ -37,42 +39,44 @@ namespace KoFrMaDaemon.ConnectionToServer
             var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
             ServiceKoFrMa.debugLog.WriteToLog("Server returned code " + httpResponse.StatusCode + " which means " + httpResponse.StatusDescription, 5);
             string result;
+            string resultBase64;
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
-                result = streamReader.ReadToEnd();
+                resultBase64 = streamReader.ReadToEnd();
             }
             ServiceKoFrMa.debugLog.WriteToLog("Performing deserialization of data that were received from the server...", 7);
+            result = this.DecodeBase64(System.Text.Encoding.UTF8, resultBase64);
             return JsonConvert.DeserializeObject<List<Task>>(result);
         }
-        public void TaskCompleted(Task task, BackupJournalObject backupJournalNew, DebugLog debugLog, bool Successfull)
-        {
-            TaskComplete completedTask = new TaskComplete()
-            {
-                IDTask = task.IDTask,
-                TimeOfCompletition = DateTime.Now,
-                DebugLog = debugLog.logReport,
-                DatFile = backupJournalNew,
-                IsSuccessfull = Successfull
-            };
+        //public void TaskCompleted(Task task, BackupJournalObject backupJournalNew, DebugLog debugLog, bool Successfull)
+        //{
+        //    TaskComplete completedTask = new TaskComplete()
+        //    {
+        //        IDTask = task.IDTask,
+        //        TimeOfCompletition = DateTime.Now,
+        //        DebugLog = debugLog.logReport,
+        //        DatFile = backupJournalNew,
+        //        IsSuccessfull = Successfull
+        //    };
 
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(ConnectionInfo.ServerURL + @"/api/Daemon/TaskCompleted");
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            {
-                string json = JsonConvert.SerializeObject(completedTask);
+        //    var httpWebRequest = (HttpWebRequest)WebRequest.Create(ConnectionInfo.ServerURL + @"/api/Daemon/TaskCompleted");
+        //    httpWebRequest.ContentType = "application/json";
+        //    httpWebRequest.Method = "POST";
+        //    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+        //    {
+        //        string json = JsonConvert.SerializeObject(completedTask);
 
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            string result;
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                result = streamReader.ReadToEnd();
-            }
-        }
+        //        streamWriter.Write(json);
+        //        streamWriter.Flush();
+        //        streamWriter.Close();
+        //    }
+        //    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        //    string result;
+        //    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        //    {
+        //        result = streamReader.ReadToEnd();
+        //    }
+        //}
         public string GetToken()
         {
             ServiceKoFrMa.debugLog.WriteToLog("Creating token request...", 7);
@@ -97,6 +101,29 @@ namespace KoFrMaDaemon.ConnectionToServer
                 result = streamReader.ReadToEnd();
             }
             return result;
+        }
+
+
+        private string EncodeBase64(System.Text.Encoding encoding, string text)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+
+            byte[] textAsBytes = encoding.GetBytes(text);
+            return System.Convert.ToBase64String(textAsBytes);
+        }
+
+        private string DecodeBase64(System.Text.Encoding encoding, string encodedText)
+        {
+            if (encodedText == null)
+            {
+                return null;
+            }
+
+            byte[] textAsBytes = System.Convert.FromBase64String(encodedText);
+            return encoding.GetString(textAsBytes);
         }
 
     }
