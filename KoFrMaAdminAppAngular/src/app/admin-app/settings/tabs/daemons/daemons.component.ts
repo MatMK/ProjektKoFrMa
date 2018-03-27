4bb4d43741ccc96dfa0d4164e36eac5b0b126fc7
Mach <pema65@seznam.cz>
2018-03-27 10:39:15 +0200

    Added more tables

--- KoFrMaAdminAppAngular/src/app/admin-app/settings/tabs/daemons/daemons.component.ts
+++ KoFrMaAdminAppAngular/src/app/admin-app/settings/tabs/daemons/daemons.component.ts
@@ -1,4 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ServerConnectionService } from '../../../server-connection/server-connection.service';
+import { tbDaemons } from '../../../server-connection/models/sql-data/data/tb-daemons.model';
+import { Data } from '../../../server-connection/data.model';
+import { DataSource } from '@angular/cdk/table';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-daemons',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DaemonsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service: ServerConnectionService, private data: Data) {
+    this.refresh();
+  }
+  displayedColumns = ['Id', 'Version', 'OS', 'PC_Unique', 'Allowed', 'LastSeen'];
+  
+  refresh() {
+    this.service.GetGettbDaemons().then(res => this.data.Daemons = res);
+  }
   ngOnInit() {
   }
+}
+export interface DaemonsTable {
+   Id: number;
+   Version: number;
+   OS: string;
+   PC_Unique: string;
+   Allowed: boolean;
+   LastSeen: Date;
+ 
 
 }
--- KoFrMaAdminAppAngular/src/app/admin-app/settings/tabs/tasks/tasks.component.ts
+++ KoFrMaAdminAppAngular/src/app/admin-app/settings/tabs/tasks/tasks.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerConnectionService } from '../../../server-connection/server-connection.service';
+import { tbTasks } from '../../../server-connection/models/sql-data/data/tb-tasks.model';
+import { Data } from '../../../server-connection/data.model';
+import { DataSource } from '@angular/cdk/table';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-tasks',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
 
+  constructor(private service: ServerConnectionService, private data: Data) {
+    this.refresh();
+  }
+  displayedColumns = ['Id', 'IDaemon', 'Task', 'TimeOfExecution', 'RepeatInJSON', 'Completed'];
+  
+  refresh() {
+    this.service.GettbTasks().then(res => this.data.Tasks = res);
+  }
   ngOnInit() {
   }
-
+}
+export interface TaskTable {
+   Id: number;
+   IdDaemon: number;
+   Task: string;
+   TimeOfExecution: Date;
+   RepeatInJSON: string;
+  Completed: boolean;
 }
