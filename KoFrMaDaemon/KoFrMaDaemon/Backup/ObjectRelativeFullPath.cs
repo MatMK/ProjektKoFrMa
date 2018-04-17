0cb82c1bba309b94397b04c537e75fe43d0c4e2e
Matěj Kocourek <matej@kocourek.cz>
2018-04-17 22:15:35 +0200

    Added some class to fix problem of full paths

---
+++ KoFrMaDaemon/KoFrMaDaemon/Backup/ObjectRelativeFullPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoFrMaDaemon.Backup
+{
+    public class ObjectRelativeFullPath
+    {
+        public string Prefix { get; set; }
+
+        public string RelativePath { get; set; }
+    }
+}
