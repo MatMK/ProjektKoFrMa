7f99f533a3d4b419f4980006ca961154c200bd3a
Matěj Kocourek <matej@kocourek.cz>
2018-06-02 12:19:20 +0200

    Added better classes for inner copying of files and folders

---
+++ KoFrMaDaemon/KoFrMaDaemon/Backup/FileCopyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoFrMaDaemon.Backup
+{
+    public class FileCopyInfo
+    {
+        /// <summary>
+        /// Info about file that would be copied
+        /// </summary>
+        public FileInfo SourceFileInfo { get; set; }
+        /// <summary>
+        /// Full path to where the file should be copied
+        /// </summary>
+        public string DestinationPath { get; set; }
+    }
+}
---
+++ KoFrMaDaemon/KoFrMaDaemon/Backup/FolderCopyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoFrMaDaemon.Backup
+{
+    public class FolderCopyInfo
+    {
+        /// <summary>
+        /// Info about folder that would be created
+        /// </summary>
+        public DirectoryInfo SourceFolderInfo { get; set; }
+        /// <summary>
+        /// Full path to where the folder should be created
+        /// </summary>
+        public string DestinationPath { get; set; }
+    }
+}
